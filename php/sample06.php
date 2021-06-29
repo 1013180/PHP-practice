e98d82413ada56fc3b31719a5e5450268c93add3
1013180 <hiroki.kojima0417@gmail.com>
2021-06-29 19:05:43 +0900

    Finish,2-17

--- php/sample06.php
+++ php/sample06.php
@@ -19,11 +19,14 @@
 <h2>Practice</h2>
 <pre>
 <?php 
-$i = 1;
+//$i = 1;
+//while ($i <= 365){
+//  print($i."\n");
+//  $i++;
+//}
 
-while ($i <= 365){
-  print($i."\n");
-  $i = $i+1;
+for ($i=1; $i<=365; $i++){
+   print($i."\n");
 }
 ?>
 </pre>
